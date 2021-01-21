283cf6cf89fed428f15e9eca75661de24143e6d7
younhong <younhong@kakao.com>
2021-01-22 00:04:15 +0900

    add image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import File from '../images/gatsby-astronaut.png'
 
 const IndexPage = () => (
   <div>
     <h1>Welcome to my website</h1>
     <p>This is a sample site for the Gatsby crash course</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <img src={File} />
+    </div>
   </div>
 )
 
